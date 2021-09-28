64dd8d140c81745ba967671069f4f8a04d377fa6
Murat <muratibmtal@gmail.com>
2021-09-28 13:55:12 +0300

    Initial commit

---
+++ src/oğrenciBilgileri/Main.java
@@ -0,0 +1,25 @@
+package oðrenciBilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Öðrenci Bilgileri");
+        System.out.print("Ýsim");
+        System.out.print(" ");
+        System.out.print("Soyisim:");
+        System.out.println("Murat Eyken");
+        System.out.print("Yaþ:");
+        System.out.println("15");
+        System.out.print("Doðum yeri:");
+        System.out.println("Bolu");
+        System.out.print("Okulu:");
+        System.out.println("Ýzzet Baysal Mesleki ve teknik Anadolu lisesi");
+        System.out.print("Bölümü:");
+        System.out.println("Biliþim Bölümü");
+        System.out.print("Sýnýfý:");
+        System.out.println("10/A");
+        
+	}
+
+}
